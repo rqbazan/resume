20431f5d1f41c1be56cd0b102abae02ba536786d
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2021-01-11 00:09:28 -0500

    fix: use abs path to included files

--- src/app/register-fonts.js
+++ src/app/register-fonts.js
@@ -1,19 +1,24 @@
+import path from 'path'
 import { Font } from '@react-pdf/renderer'
+
+function src(filename) {
+  return path.join(__dirname, 'assets', 'fonts', filename)
+}
 
 Font.register({
   family: 'Quicksand',
   fonts: [
-    { src: './fonts/quicksand-v20-latin-regular.ttf' },
-    { src: './fonts/quicksand-v20-latin-300.ttf', fontWeight: 300 },
-    { src: './fonts/quicksand-v20-latin-600.ttf', fontWeight: 600 }
+    { src: src('quicksand-v20-latin-regular.ttf') },
+    { src: src('quicksand-v20-latin-300.ttf'), fontWeight: 300 },
+    { src: src('quicksand-v20-latin-600.ttf'), fontWeight: 600 }
   ]
 })
 
 Font.register({
   family: 'OpenSans',
   fonts: [
-    { src: './fonts/open-sans-v17-latin-regular.ttf' },
-    { src: './fonts/open-sans-v17-latin-600.ttf', fontWeight: 600 },
-    { src: './fonts/open-sans-v17-latin-700.ttf', fontWeight: 700 }
+    { src: src('open-sans-v17-latin-regular.ttf') },
+    { src: src('open-sans-v17-latin-600.ttf'), fontWeight: 600 },
+    { src: src('open-sans-v17-latin-700.ttf'), fontWeight: 700 }
   ]
 })

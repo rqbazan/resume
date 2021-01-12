d37ad48c3db5f44ca8d044ebb3de25a791f68449
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2021-01-12 17:27:26 -0500

    fix: add vercel-build

--- api/resume.js
+++ api/resume.js
@@ -1,21 +1,8 @@
-import fs from 'fs'
-import path from 'path'
-//import { createElement } from 'react'
-//import { App, renderToStream } from './_lib'
+import { createElement } from 'react'
+import { App, renderToStream } from './_lib'
 
 export default async (req, res) => {
-  //const stream = await renderToStream(createElement(App))
-  //res.setHeader('Content-Type', 'application/pdf')
-  //stream.pipe(res)
-
-  const [files, parentFiles] = await Promise.all([
-    fs.promises.readdir(__dirname),
-    fs.promises.readdir(path.resolve(__dirname, '..'))
-  ])
-
-  res.json({
-    pwd: __dirname,
-    files,
-    parentFiles
-  })
+  const stream = await renderToStream(createElement(App))
+  res.setHeader('Content-Type', 'application/pdf')
+  stream.pipe(res)
 }

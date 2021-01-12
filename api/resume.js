55df6818c943d7aafe066cc3e37b21e371b1139d
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2021-01-12 16:26:48 -0500

    chore: explorer files inside lambda files

--- api/resume.js
+++ api/resume.js
@@ -1,8 +1,21 @@
-import { createElement } from 'react'
-import { App, renderToStream } from './_lib'
+import fs from 'fs'
+import path from 'path'
+//import { createElement } from 'react'
+//import { App, renderToStream } from './_lib'
 
 export default async (req, res) => {
-  const stream = await renderToStream(createElement(App))
-  res.setHeader('Content-Type', 'application/pdf')
-  stream.pipe(res)
+  //const stream = await renderToStream(createElement(App))
+  //res.setHeader('Content-Type', 'application/pdf')
+  //stream.pipe(res)
+
+  const [files, parentFiles] = await Promise.all([
+    fs.promises.readdir(__dirname),
+    fs.promises.readdir(path.resolve(__dirname, '..'))
+  ])
+
+  res.json({
+    pwd: __dirname,
+    files,
+    parentFiles
+  })
 }
